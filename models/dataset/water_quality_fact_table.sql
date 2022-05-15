d546feddf82a6e3134e1a85287afc2cf61e8b4bf
sangie.cheung@baruchmail.cuny.edu <sangie.cheung@baruchmail.cuny.edu>
2022-05-15 23:59:02 +0000

    Fixed column names

--- models/dataset/water_quality_fact_table.sql
+++ models/dataset/water_quality_fact_table.sql
@@ -17,6 +17,6 @@ joined as (
     left join location on water_quality.sample_site = location.sample_site AND water_quality.latitude = location.latitude AND water_quality.longitude = location.longitude 
     left join sample_information on water_quality.sample_number = sample_information.sample_number AND water_quality.sample_class = sample_information.sample_class
 )
-select *, count(*) as Number_of_Complaints
+select *, count(*) as Number_of_Samples
 from joined
 group by sample_information_dim_id, date_dim_id, location_dim_id, Residual_Free_Chlorine, Turbidity, Fluoride, Coliform, E_coli
